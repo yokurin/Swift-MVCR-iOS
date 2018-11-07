77f4606084dc9a65e3336acb63fad24ca232c36b
林　翼 <tsubasa_hayashi@r.recruit.co.jp>
2018-11-07 15:46:17 +0900

    Initial Commit

---
+++ SwiftMVCR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftMVCR
+//
+//  Created by 林　翼 on 2018/11/07.
+//  Copyright © 2018年 Tsubasa Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
